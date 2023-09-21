71ab04c92d83935e7720bf552b3b5295f4434ff6
MmMapIoSpace <krsgroup18@gmail.com>
2023-09-21 17:03:05 +0700

    #

--- UCMapper/main.c
+++ UCMapper/main.c
@@ -5,21 +5,21 @@
 PDEVICE_DRIVER_OBJECT Driver = NULL;
 #pragma data_seg(pop)
 
-NTSTATUS wmain(_In_ LONG ArgumentCount, _In_ LPWSTR* Argument)
+int __cdecl wmain(_In_ int argc, _In_ wchar_t** argv)
 {
     NTSTATUS Status;
     PVOID ImageBase;
     SIZE_T ImageSize;
     LPWSTR DriverPath;
 
-    if (ArgumentCount != 2) {
+    if (argc != 2) {
         Status = STATUS_INVALID_PARAMETER;
         DEBUG_PRINT_NTERROR(STATUS_INVALID_PARAMETER);
-        DEBUG_PRINT("[!] invalid arguments.\r\n\t%ws <Driver Path>\r\n", Argument[0]);
+        DEBUG_PRINT("[!] invalid arguments.\r\n\t%ws <Driver Path>\r\n", argv[0]);
         return Status;
     }
 
-    DriverPath = Argument[1];
+    DriverPath = argv[1];
     Status     = RtlFileMapImage(DriverPath, &ImageBase, &ImageSize);
     if NT_ERROR (Status) {
         DEBUG_PRINT_NTERROR(Status);
