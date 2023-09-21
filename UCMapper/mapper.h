d34474d232fb83abbf4dc8ce0dccf93526e25e49
MmMapIoSpace <krsgroup18@gmail.com>
2023-09-21 17:14:27 +0700

    #

--- UCMapper/mapper.h
+++ UCMapper/mapper.h
@@ -4,6 +4,18 @@
 // Prototype
 //
 
+typedef struct _MDL* PMDL;
+typedef struct _EPROCESS* PEPROCESS;
+
+typedef struct _MM_COPY_ADDRESS
+{
+    union
+    {
+        PVOID VirtualAddress;
+        PHYSICAL_ADDRESS PhysicalAddress;
+    };
+} MM_COPY_ADDRESS, *PMMCOPY_ADDRESS;
+
 typedef enum _MEMORY_CACHING_TYPE
 {
     MmNonCached,
@@ -23,9 +35,64 @@ typedef enum _MM_PAGE_PRIORITY
     HighPagePriority   = 32
 } MM_PAGE_PRIORITY;
 
+typedef enum _LOCK_OPERATION
+{
+    IoReadAccess,
+    IoWriteAccess,
+    IoModifyAccess
+} LOCK_OPERATION;
+
 #define MdlMappingNoWrite       0x80000000 // Create the mapping as nowrite
 #define MdlMappingNoExecute     0x40000000 // Create the mapping as noexecute
 #define MdlMappingWithGuardPtes 0x20000000 // Create the mapping with guard PTEs
+
+
+typedef NTSTATUS (*ZwClose_t)(HANDLE);
+typedef NTSTATUS (*PsGetThreadExitStatus_t)(_In_ PVOID Thread);
+typedef LONG_PTR (*ObfDereferenceObject_t)(_In_ PVOID Object);
+typedef PVOID (*ExAllocatePool2_t)(_In_ ULONGLONG Flags, _In_ SIZE_T NumberOfBytes, _In_ ULONG Tag);
+typedef void (*ExFreePoolWithTag_t)(PVOID P, ULONG Tag);
+typedef void (*MmFreePagesFromMdl_t)(PVOID MemoryDescriptorList);
+typedef PIMAGE_NT_HEADERS (*RtlImageNtHeader_t)(_In_ PVOID Base);
+typedef NTSTATUS (*PsTerminateSystemThread_t)(NTSTATUS ExitStatus);
+typedef NTSTATUS (*PDRIVER_INITIALIZE)(PDRIVER_OBJECT, PUNICODE_STRING);
+typedef PVOID (*RtlFindExportedRoutineByName_t)(_In_ PVOID BaseOfImage, _In_ PSTR RoutineName);
+typedef void (*RtlFreeUnicodeString_t)(PUNICODE_STRING UnicodeString);
+typedef PVOID (*MmGetSystemRoutineAddress_t)(PUNICODE_STRING SystemRoutineName);
+typedef NTSTATUS (*MmProtectMdlSystemAddress_t)(PVOID MemoryDescriptorList, ULONG NewProtect);
+typedef void (*IoFreeMdl_t)(PVOID Mdl);
+typedef VOID (*MmUnmapLockedPages_t)(_In_ PVOID BaseAddress, _Inout_ PMDL MemoryDescriptorList);
+typedef VOID (*MmUnlockPages_t)(_Inout_ PMDL MemoryDescriptorList);
+typedef LONG (*KeSetEvent_t)(IN OUT PRKEVENT Event, IN KPRIORITY Increment, IN BOOLEAN Wait);
+typedef NTSTATUS (*MmMapViewInSystemSpace_t)(IN PVOID Section, PVOID* MappedBase, PSIZE_T ViewSize);
+typedef NTSTATUS (*MmUnmapViewInSystemSpace_t)(PVOID MappedBase);
+typedef NTSTATUS (*PsLookupProcessByProcessId_t)(IN HANDLE ProcessId, OUT PEPROCESS* Process);
+typedef void (*MmUnmapIoSpace_t)(IN PVOID BaseAddress, IN SIZE_T NumberOfBytes);
+
+typedef PVOID (*IoAllocateMdl_t)(
+    __drv_aliasesMem PVOID VirtualAddress,
+    ULONG Length,
+    BOOLEAN SecondaryBuffer,
+    BOOLEAN ChargeQuota,
+    PVOID Irp);
+
+typedef BOOLEAN (*RtlEqualUnicodeString_t)(
+    PCUNICODE_STRING String1,
+    PCUNICODE_STRING String2,
+    BOOLEAN CaseInSensitive);
+
+typedef NTSTATUS (*RtlAnsiStringToUnicodeString_t)(
+    PUNICODE_STRING DestinationString,
+    PCANSI_STRING SourceString,
+    BOOLEAN AllocateDestinationString);
+
+typedef VOID (
+    *RtlInitAnsiString_t)(PANSI_STRING DestinationString, __drv_aliasesMem PCSZ SourceString);
+
+typedef void* (*memcpy_t)(
+    _Out_writes_bytes_all_(_Size) void* _Dst,
+    _In_reads_bytes_(_Size) const void* _Src,
+    _In_ size_t _Size);
 
 typedef NTSTATUS (*PsCreateSystemThread_t)(
     _Out_ PHANDLE ThreadHandle,
@@ -50,10 +117,12 @@ typedef NTSTATUS (*KeWaitForSingleObject_t)(
     _In_ __drv_strictType(KPROCESSOR_MODE / enum _MODE, __drv_typeConst) KPROCESSOR_MODE WaitMode,
     _In_ BOOLEAN Alertable,
     _In_opt_ PLARGE_INTEGER Timeout);
+
 typedef NTSTATUS (*KeDelayExecutionThread_t)(
     _In_ KPROCESSOR_MODE WaitMode,
     _In_ BOOLEAN Alertable,
     _In_ PLARGE_INTEGER Interval);
+
 typedef PVOID (*MmAllocatePagesForMdlEx_t)(
     PHYSICAL_ADDRESS LowAddress,
     PHYSICAL_ADDRESS HighAddress,
@@ -61,11 +130,13 @@ typedef PVOID (*MmAllocatePagesForMdlEx_t)(
     SIZE_T TotalBytes,
     MEMORY_CACHING_TYPE CacheType,
     ULONG Flags);
+
 typedef PVOID (*RtlImageDirectoryEntryToData_t)(
     _In_ PVOID BaseOfImage,
     _In_ BOOLEAN MappedAsImage,
     _In_ USHORT DirectoryEntry,
     _Out_ PULONG Size);
+
 typedef NTSTATUS (*ObReferenceObjectByHandle_t)(
     _In_ HANDLE Handle,
     _In_ ACCESS_MASK DesiredAccess,
@@ -73,40 +144,49 @@ typedef NTSTATUS (*ObReferenceObjectByHandle_t)(
     _In_ KPROCESSOR_MODE AccessMode,
     _Out_ PVOID* Object,
     _Out_opt_ PVOID HandleInformation);
-typedef NTSTATUS (*ZwClose_t)(HANDLE);
-typedef NTSTATUS (*PsGetThreadExitStatus_t)(_In_ PVOID Thread);
-typedef LONG_PTR (*ObfDereferenceObject_t)(_In_ PVOID Object);
-typedef PVOID (*ExAllocatePool2_t)(_In_ ULONGLONG Flags, _In_ SIZE_T NumberOfBytes, _In_ ULONG Tag);
-typedef void* (*memcpy_t)(
-    _Out_writes_bytes_all_(_Size) void* _Dst,
-    _In_reads_bytes_(_Size) const void* _Src,
-    _In_ size_t _Size);
-typedef void (*ExFreePoolWithTag_t)(PVOID P, ULONG Tag);
-typedef void (*MmFreePagesFromMdl_t)(PVOID MemoryDescriptorList);
-typedef PIMAGE_NT_HEADERS (*RtlImageNtHeader_t)(_In_ PVOID Base);
-typedef NTSTATUS (*PsTerminateSystemThread_t)(NTSTATUS ExitStatus);
-typedef NTSTATUS (*PDRIVER_INITIALIZE)(PDRIVER_OBJECT, PUNICODE_STRING);
-typedef PVOID (*RtlFindExportedRoutineByName_t)(_In_ PVOID BaseOfImage, _In_ PSTR RoutineName);
-typedef VOID (
-    *RtlInitAnsiString_t)(PANSI_STRING DestinationString, __drv_aliasesMem PCSZ SourceString);
-typedef NTSTATUS (*RtlAnsiStringToUnicodeString_t)(
-    PUNICODE_STRING DestinationString,
-    PCANSI_STRING SourceString,
-    BOOLEAN AllocateDestinationString);
-typedef BOOLEAN (*RtlEqualUnicodeString_t)(
-    PCUNICODE_STRING String1,
-    PCUNICODE_STRING String2,
-    BOOLEAN CaseInSensitive);
-typedef void (*RtlFreeUnicodeString_t)(PUNICODE_STRING UnicodeString);
-typedef PVOID (*MmGetSystemRoutineAddress_t)(PUNICODE_STRING SystemRoutineName);
-typedef NTSTATUS (*MmProtectMdlSystemAddress_t)(PVOID MemoryDescriptorList, ULONG NewProtect);
-typedef PVOID (*IoAllocateMdl_t)(
-    __drv_aliasesMem PVOID VirtualAddress,
-    ULONG Length,
-    BOOLEAN SecondaryBuffer,
-    BOOLEAN ChargeQuota,
-    PVOID Irp);
-typedef void (*IoFreeMdl_t)(PVOID Mdl);
+
+typedef VOID (*MmProbeAndLockPages_t)(
+    _Inout_ PMDL MemoryDescriptorList,
+    _In_ KPROCESSOR_MODE AccessMode,
+    _In_ LOCK_OPERATION Operation);
+
+typedef NTSTATUS (*ZwOpenEvent_t)(
+    OUT PHANDLE EventHandle,
+    IN ACCESS_MASK DesiredAccess,
+    IN POBJECT_ATTRIBUTES ObjectAttributes);
+
+typedef NTSTATUS (*ZwCreateSection_t)(
+    OUT PHANDLE SectionHandle,
+    IN ACCESS_MASK DesiredAccess,
+    IN OPTIONAL POBJECT_ATTRIBUTES ObjectAttributes,
+    IN OPTIONAL PLARGE_INTEGER MaximumSize,
+    IN ULONG SectionPageProtection,
+    IN ULONG AllocationAttributes,
+    IN OPTIONAL HANDLE FileHandle);
+
+typedef NTSTATUS (*ZwOpenFile_t)(
+    OUT PHANDLE FileHandle,
+    IN ACCESS_MASK DesiredAccess,
+    IN POBJECT_ATTRIBUTES ObjectAttributes,
+    OUT PIO_STATUS_BLOCK IoStatusBlock,
+    IN ULONG ShareAccess,
+    IN ULONG OpenOptions);
+
+typedef NTSTATUS (*MmCopyMemory_t)(
+    IN PVOID TargetAddress,
+    IN MM_COPY_ADDRESS SourceAddress,
+    IN SIZE_T NumberOfBytes,
+    IN ULONG Flags,
+    OUT PSIZE_T NumberOfBytesTransferred);
+
+typedef PVOID (*MmMapIoSpaceEx_t)(
+    IN PHYSICAL_ADDRESS PhysicalAddress,
+    IN SIZE_T NumberOfBytes,
+    IN ULONG Protect);
+
+typedef VOID (*RtlInitUnicodeString_t)(
+    OUT PUNICODE_STRING DestinationString,
+    IN OPTIONAL __drv_aliasesMem PCWSTR SourceString);
 
 //
 // Structure Data Type
@@ -114,20 +194,35 @@ typedef void (*IoFreeMdl_t)(PVOID Mdl);
 
 typedef struct _KERNEL_IMPORT_TABLE
 {
+#if 1
     PLIST_ENTRY PsLoadedModuleList;
+    PEPROCESS PsInitialSystemProcess;
     memcpy_t memcpy;
     PVOID memset;
+    KeSetEvent_t KeSetEvent;
+    MmMapViewInSystemSpace_t MmMapViewInSystemSpace;
+    MmUnmapViewInSystemSpace_t MmUnmapViewInSystemSpace;
+    MmCopyMemory_t MmCopyMemory;
+    MmMapIoSpaceEx_t MmMapIoSpaceEx;
+    MmUnmapIoSpace_t MmUnmapIoSpace;
+    PsLookupProcessByProcessId_t PsLookupProcessByProcessId;
+    RtlInitUnicodeString_t RtlInitUnicodeString;
+    ZwOpenFile_t ZwOpenFile;
+    ZwCreateSection_t ZwCreateSection;
+    ZwOpenEvent_t ZwOpenEvent;
     MmGetSystemRoutineAddress_t MmGetSystemRoutineAddress;
     MmAllocatePagesForMdlEx_t MmAllocatePagesForMdlEx;
     MmFreePagesFromMdl_t MmFreePagesFromMdl;
     MmMapLockedPagesSpecifyCache_t MmMapLockedPagesSpecifyCache;
+    MmUnmapLockedPages_t MmUnmapLockedPages;
+    MmProbeAndLockPages_t MmProbeAndLockPages;
     MmProtectMdlSystemAddress_t MmProtectMdlSystemAddress;
+    MmUnlockPages_t MmUnlockPages;
     KeWaitForSingleObject_t KeWaitForSingleObject;
     KeDelayExecutionThread_t KeDelayExecutionThread;
     ExAllocatePool2_t ExAllocatePool2;
     ExFreePoolWithTag_t ExFreePoolWithTag;
     RtlImageNtHeader_t RtlImageNtHeader;
-    PVOID RtlInitUnicodeString;
     RtlInitAnsiString_t RtlInitAnsiString;
     RtlAnsiStringToUnicodeString_t RtlAnsiStringToUnicodeString;
     RtlEqualUnicodeString_t RtlEqualUnicodeString;
@@ -142,7 +237,53 @@ typedef struct _KERNEL_IMPORT_TABLE
     ZwClose_t ZwClose;
     IoAllocateMdl_t IoAllocateMdl;
     IoFreeMdl_t IoFreeMdl;
+#else
+    PVOID PsLoadedModuleList;
+    PVOID PsInitialSystemProcess;
+    PVOID memcpy;
+    PVOID memset;
+    PVOID KeSetEvent;
+    PVOID MmMapViewInSystemSpace;
+    PVOID MmUnmapViewInSystemSpace;
+    PVOID MmCopyMemory;
+    PVOID MmMapIoSpaceEx;
+    PVOID MmUnmapIoSpace;
+    PVOID PsLookupProcessByProcessId;
+    PVOID RtlInitUnicodeString;
+    PVOID ZwOpenFile;
+    PVOID ZwCreateSection;
+    PVOID ZwOpenEvent;
+    PVOID MmGetSystemRoutineAddress;
+    PVOID MmAllocatePagesForMdlEx;
+    PVOID MmFreePagesFromMdl;
+    PVOID MmMapLockedPagesSpecifyCache;
+    PVOID MmUnmapLockedPages;
+    PVOID MmProbeAndLockPages;
+    PVOID MmProtectMdlSystemAddress;
+    PVOID MmUnlockPages;
+    PVOID KeWaitForSingleObject;
+    PVOID KeDelayExecutionThread;
+    PVOID ExAllocatePool2;
+    PVOID ExFreePoolWithTag;
+    PVOID RtlImageNtHeader;
+    PVOID RtlInitAnsiString;
+    PVOID RtlAnsiStringToUnicodeString;
+    PVOID RtlEqualUnicodeString;
+    PVOID RtlFreeUnicodeString;
+    PVOID RtlImageDirectoryEntryToData;
+    PVOID RtlFindExportedRoutineByName;
+    PVOID ObReferenceObjectByHandle;
+    PVOID ObfDereferenceObject;
+    PVOID PsCreateSystemThread;
+    PVOID PsTerminateSystemThread;
+    PVOID PsGetThreadExitStatus;
+    PVOID ZwClose;
+    PVOID IoAllocateMdl;
+    PVOID IoFreeMdl;
+#endif
 } KERNEL_IMPORT_TABLE, *PKERNEL_IMPORT_TABLE;
+
+C_ASSERT(sizeof(KERNEL_IMPORT_TABLE) == 336);
 
 typedef struct _MAPPER_EXECUTOR_CONTEXT
 {
